3da2d814e82c4f37525b0b2d24e6802dfc76c71b
clownami <roccoyt228@gmail.com>
2022-06-18 13:35:37 +0300

    Update

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentTrafficLight = TrafficLights.red
+    
+    @IBOutlet var views: [UIView]!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        button.configuration = setupUpButton(with: "START")
     }
-
-
+    
+    @IBAction func changeColor() {
+        button.configuration = setupUpButton(with: "NEXT")
+        
+        switch currentTrafficLight {
+        case .red:
+            views[2].alpha = 0.3
+            views[0].alpha = 1
+            currentTrafficLight = .yellow
+        case .yellow:
+            views[0].alpha = 0.3
+            views[1].alpha = 1
+            currentTrafficLight = .green
+        case .green:
+            views[1].alpha = 0.3
+            views[2].alpha = 1
+            currentTrafficLight = .red
+        }
+    }
+    
+    private func setupUpButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        return buttonConfiguration
+    }
+    
+    private func setupViews() {
+        for view in views {
+            view.layer.cornerRadius = view.frame.height / 2
+            view.alpha = 0.3
+        }
+    }
+    
+    enum TrafficLights {
+        case red, yellow, green
+    }
 }
 
